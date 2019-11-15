ccfd84f0e135f025fb0aa6f5b61997be2d61d23c
Salar Moghaddam <salarmgh@gmail.com>
2019-11-15 16:31:19 +0330

    Change sample pick

--- src/containers/Product/Product.tsx
+++ src/containers/Product/Product.tsx
@@ -11,7 +11,7 @@ const Product = () => {
         <div className="columns">
           <div className="column">
             <Image
-              image="https://bulma.io/images/placeholders/256x256.png"
+              image="https://instagram.fevn1-1.fna.fbcdn.net/vp/220ac8fcc232c397367a72a520b0bf24/5E4759DA/t51.2885-15/e35/s1080x1080/74448490_2556314537988766_6964396219630299901_n.jpg?_nc_ht=instagram.fevn1-1.fna.fbcdn.net&_nc_cat=110"
               size="square"
             />
           </div>
