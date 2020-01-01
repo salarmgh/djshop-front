29c410f81fa51132023701e97310628741101639
Salar Moghaddam <salarmgh@gmail.com>
2020-01-01 19:02:35 +0330

    Add query string processor

--- src/containers/Category/Category.tsx
+++ src/containers/Category/Category.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Page from "../Layouts/Page";
 import Banner from "../../components/Banner/Banner";
@@ -6,104 +6,105 @@ import CategoryProducts from "../CategoryProducts/CategoryProducts";
 import Filters from "../Filters/Filters";
 import PriceSlider from "../../components/PriceSlider/PriceSlider";
 
+
 const Category = () => {
-    const backendUrl = process.env.REACT_APP_BACKEND_BASE_URL;
-    const [category, setCategory] = useState({
+  const backendUrl = process.env.REACT_APP_BACKEND_BASE_URL;
+  const [filterAttributes, setFilterAttributes] = useState({});
+  const [category, setCategory] = useState({
     id: 0,
     image: "https://bulma.io/images/placeholders/1280x960.png",
     name: "category",
     attributes: [
-        {
-            id: 0,
-            name: "attribute",
-            attributes: [
-                {
-                    "value": "value",
-                    "attribute": 0
-                }
-            ]
-        }
+      {
+        id: 0,
+        name: "attribute",
+        attributes: [
+          {
+            "value": "value",
+            "attribute": 0
+          }
+        ]
+      }
     ]
   });
 
-    const [attributes, setAttributes] = useState([{
+  const [attributes, setAttributes] = useState([{
     id: 0,
     name: "attribute",
     attributes: [
-        {
-            value: "value"
-        }
+      {
+        value: "value"
+      }
     ]
   }]);
 
-    const [products, setProducts] = useState([{
-        _source: {
-        name: "attribute",
-        price: 1234,
-        product: {
-            title: "title",
-            description: "desc",
-            slug: "/",
-            featured: true
-        },
-        images: ["https://bulma.io/images/placeholders/1280x960.png"]
-      }
-    }]);
+  const [products, setProducts] = useState([{
+    _source: {
+      name: "attribute",
+      price: 1234,
+      product: {
+        title: "title",
+        description: "desc",
+        slug: "/",
+        featured: true
+      },
+      images: ["https://bulma.io/images/placeholders/1280x960.png"]
+    }
+  }]);
 
-    useEffect(() => {
-        axios.get(`${backendUrl}/categories/necklace/`).then(({ data }) => {
-            setCategory(data);
-            setAttributes(data.attributes);
-        });
-    }, [backendUrl]);
-    
-    const [filterAttributes, setFilterAttributes] = useState({});
-    
-    useEffect(() => {
-        axios.get(`${backendUrl}/search/?categories=Necklace`).then(({ data }) => {
-            setProducts(data);
-        });
-    }, [backendUrl]);
+  useEffect(() => {
+    axios.get(`${backendUrl}/categories/necklace/`).then(({ data }) => {
+      setCategory(data);
+      setAttributes(data.attributes);
+    });
+  }, [backendUrl]);
 
-    const filterCheckHandler = (
-        event: any
-        ) => {
-            const attributes = {...filterAttributes};
-            const key = event.target.value.split("|")[0];
-            const value = event.target.value.split("|")[1];
-            if (event.target.checked === true) {
-                if (typeof attributes[key] == 'undefined') {
-                   attributes[key] = [value];
-                } else {
-                   attributes[key].push(value);
-                }
-            } else {
-                 attributes[key].splice( attributes[key].indexOf(value), 1 );
-                 if (attributes[key].length === 0) {
-                    delete attributes[key];
-                 }
-            }
-            setFilterAttributes(attributes);
-        };
 
-    return (
-      <Page>
-        <div className="container is-fluid">
-          <Banner image={category.image} />
-          <div className="columns">
-            <div className="column is-one-quarter">
-              <Filters onChange={filterCheckHandler} attributes={attributes} />
-              <PriceSlider min={1000} max={100000} defaultMin={10000} defaultMax={50000} />
-            </div>
-            <div className="column">
-              <div className="columns is-multiline">
-                  <CategoryProducts products={products} />
-              </div>
+
+  useEffect(() => {
+    axios.get(`${backendUrl}/search/?categories=Necklace`).then(({ data }) => {
+      setProducts(data);
+    });
+  }, [backendUrl]);
+
+  const filterCheckHandler = (
+    event: any
+  ) => {
+    const attributes = { ...filterAttributes };
+    const key = event.target.value.split("|")[0];
+    const value = event.target.value.split("|")[1];
+    if (event.target.checked === true) {
+      attributes[key] = value;
+    } else {
+      delete attributes[key];
+    }
+    setFilterAttributes(attributes);
+    for (let key in attributes) {
+      console.log(key + "|" + attributes[key]);
+    }
+  };
+
+  const queryString = require('query-string');
+
+
+  return (
+    <Page>
+      <div className="container is-fluid">
+        <Banner image={category.image} />
+        <div className="columns">
+          <div className="column is-one-quarter">
+            <Filters onChange={filterCheckHandler} attributes={attributes} />
+            <PriceSlider min={1000} max={100000} defaultMin={10000} defaultMax={50000} />
+          </div>
+          <div className="column">
+            <div className="columns is-multiline">
+              <CategoryProducts products={products} />
             </div>
           </div>
         </div>
-      </Page>
-    );
+      </div>
+    </Page>
+  );
 }
 
 export default Category;
